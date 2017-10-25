a857a7c1953f991b3d4bc61dc7216b37c69dd256
fishyFrogFace <camillmg@stud.ntnu.no>
2017-10-25 15:28:49 +0200

    Withdraw and deposit to account 1

--- task1-project/src/main/scala/Account.scala
+++ task1-project/src/main/scala/Account.scala
@@ -1,5 +1,22 @@
+import exceptions.{IllegalAmountException, NoSufficientFundsException}
+
 class Account(initialBalance: Double, val uid: Int = Bank getUniqueId) {
-  def withdraw(amount: Double): Unit = ??? // Implement
-  def deposit(amount: Double): Unit = ??? // Implement
-  def getBalanceAmount: Double = ??? // Implement
+  private var balance = initialBalance
+
+  def withdraw(amount: Double): Unit =
+    if (amount < 0)
+      throw new IllegalAmountException("negative amount")
+    else if (initialBalance-amount < 0)
+      throw new NoSufficientFundsException("not enough moneyz")
+    else
+      balance = balance-amount
+
+  def deposit(amount: Double): Unit =
+    if (amount < 0)
+      throw new IllegalAmountException("negative amount")
+    else
+      balance = balance+amount
+
+  def getBalanceAmount: Double = return balance
+
 }
--- task1-project/src/main/scala/Bank.scala
+++ task1-project/src/main/scala/Bank.scala
@@ -2,10 +2,14 @@ object Bank {
 
   private var idCounter: Int = 0
 
-  def transaction(from: Account, to: Account, amount: Double): Unit = ??? // Implement
+  def transaction(from: Account, to: Account, amount: Double): Unit = {
+    from.withdraw(amount)
+    to.deposit(amount)
+  }
 
-  def getUniqueId: Int = {
-    idCounter += 1 // Can this be improved?
+
+  def getUniqueId: Int = this.synchronized{
+    idCounter += 1
     idCounter
   }
 
